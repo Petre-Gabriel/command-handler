4cbbf896baa9227f20e47b9551ac7c8a34c31d1b
Petre Gabriel <gabegfxm@gmail.com>
2022-06-18 16:50:23 +0300

    Updated example.ts file

--- example.ts
+++ example.ts
@@ -13,7 +13,7 @@ GameCommandHandler.register("wishper", () => {}, [
 ]); /* The user can use the whisper command only if he has the LOGGED_IN flag. */
 
 GameCommandHandler.register(
-  "ping %d<You need to specify the admin level> %u<The user you provided is not online> %s<You must give a reason for the promotion> %d<Test>",
+  "ping %d<You need to specify the admin level> %u<The user you provided is not online> %s<You must give a reason for the promotion>",
   (user, adminLevel, userName, reason) => {
     console.log(
       `${user.name} has promoted ${userName} to admin level ${adminLevel}, reason: ${reason}`
